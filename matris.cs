6af0f9337638b5b426c3b5f141633c0d11b42ee3
Emir Öztürk <62071518+emirozturk1@users.noreply.github.com>
2021-01-10 03:44:25 +0300

    Update matris.cs

--- matris.cs
+++ matris.cs
@@ -5,36 +5,36 @@ namespace Matris
     {
         static void Main(string[] args)
         {
-            int secim = 0; //while dongusu'nun devami icin kullanicidan deger alacak bir degisken tanimlandi.
+            int secim = 0; 
 
             while (secim < 4)
             {
                 Console.Clear();
                 Console.WriteLine("..:: Islemler ::.. \n 1-Matris Islemleri \n 2-String Islemleri \n 3-Cikis");
                 Console.Write("Seciminiz:");
-                secim = int.Parse(Console.ReadLine());//secim degeri alindi.
+                secim = int.Parse(Console.ReadLine());
               
-                if (secim == 1)//secim degiskeninin kontrolu yapildi.
+                if (secim == 1)
                 {
                     int satirsayi;
                     Console.Clear();
                     Console.Write("..:: Matris Islemleri ::.. \n Matrisin satir sayisini giriniz(1-10 arasinda):");
-                    satirsayi = int.Parse(Console.ReadLine());//kullanicidan matris icin satir sayisi alindi.
-                    int[,] matris = new int[satirsayi, satirsayi];//2 boyutlu matris adinda bir dizi olusturuldu.
+                    satirsayi = int.Parse(Console.ReadLine());
+                    int[,] matris = new int[satirsayi, satirsayi];
          
                     for (int i = 0; i < satirsayi; i++)
                     {
                         for (int k = 0; k < satirsayi; k++)
                         {
                             Console.Write("[{0},{1}]=", i + 1, k + 1);
-                            int deger = int.Parse(Console.ReadLine()); //Kullanicidan matrisin degerleri alindi.              
+                            int deger = int.Parse(Console.ReadLine());              
                             matris[k, i] = deger;
                         }
                         Console.WriteLine();
                     }
 
                     int[] enbuyukler = new int[satirsayi];
-                    int[] toplamlar = new int[satirsayi];//enbuyukler ve toplamlar adinda dizi olusturuldu.
+                    int[] toplamlar = new int[satirsayi];
                     for (int i = 0; i < satirsayi; i++)
                     {
                         int toplam = 0;
@@ -54,9 +54,7 @@ namespace Matris
                     }
 
                     Console.Write(" 1-Satir En Buyuk \n 2-Satir Toplam \n Seciminiz:");
-                    //Bu for dongusunda her satirdaki elemlar arasinda tek tek gezinip if kontroluyle satirdaki en buyuk deger bulundu.
-                    //Ayni zamanda satirdaki toplamlar bulundu.
-                    int secim2 = int.Parse(Console.ReadLine());//kullanici secimi icin yine bir secim degiskeni olusturuldu.
+                    int secim2 = int.Parse(Console.ReadLine());
                     if (secim2 == 1)
                     {
                         for (int k = 0; k < satirsayi; k++)
@@ -75,7 +73,7 @@ namespace Matris
                     }
                     Console.WriteLine("Devam Icin Tusa Basin");
                     Console.ReadKey();
-                }//if ve else kontrol yapisiyla ekrana istenen degerler yazdirildi.
+                }
                 else if (secim == 2)
                 {
                     Console.Clear();
@@ -88,11 +86,11 @@ namespace Matris
                     foreach (char i in islem)
                     {
                         if (i == aharf)
-                            sayac++; //foreach dongusu ile islem stringi'nin icidneki istenen i harfinin kac tane bulundugu hesaplandi.
+                            sayac++; 
                     }
-                    int aharfkacinci1 = islem.IndexOf(aharf);//aranan harfin bastan kacinci oldugu bulundu.
-                    int aharfkacinci2 = islem.LastIndexOf(aharf);//aranan harfin sondan kacinci oldugu bulundu.
-                    int boyut = aharfkacinci2 - aharfkacinci1;//aradaki fark alindi.
+                    int aharfkacinci1 = islem.IndexOf(aharf);
+                    int aharfkacinci2 = islem.LastIndexOf(aharf);
+                    int boyut = aharfkacinci2 - aharfkacinci1;
                     if (sayac == 0)
                     {
                         Console.WriteLine("Istediginiz Harf Bulunamamistir");
@@ -107,9 +105,8 @@ namespace Matris
                         else if (sayac == 2)
                         {
                             Console.WriteLine("***Lutfen Islem Yapmak Icin Tuslayiniz***\n1-Ara Metni Tersten Yaz \n2-Ara Metni Tekrarli Yaz");
-                            int secim3 = int.Parse(Console.ReadLine());//kullanici secimi icin deger alindi.
-                            string ters = islem.Substring(aharfkacinci1 + 1, boyut - 1);/*substring fonksiyonu ile istenen string ters adinda bir
-                           degiskene atandi. */
+                            int secim3 = int.Parse(Console.ReadLine());
+                            string ters = islem.Substring(aharfkacinci1 + 1, boyut - 1);
                             if (secim3 == 1)
                             {
                                 for (int i = ters.Length - 1; i >= 0; i--)
@@ -138,6 +135,7 @@ namespace Matris
                 }
 
                 else
+                    
                 {
                     secim = 0;
                     Console.WriteLine("Yanlis Secim Yaptiniz \n Devam Etmek Icin Bir Tusa Basiniz");
